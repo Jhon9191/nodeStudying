69f8aee154f1b47ea70d96e700d516ad8f8d1eff
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-13 06:22:41 -0300

    Express router and controllers

--- apiNode/server.js
+++ apiNode/server.js
@@ -1,28 +1,10 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
 
 app.use(express.urlencoded({extended : true}));
 
-app.get('/', (req, res)=>{
-    res.send(`
-    <form action="/" method="post">
-    Nome: <input type="text" name="nome"/>
-    <button>Enviar</button>
-    </form>
-    `);
-})
-
-app.get('/testes/:idUsuarios?/:param', (req, res)=>{
-    console.log(req.params);
-    console.log(req.query);
-    res.send(req.params);
-})
-
-app.post('/', (req, res)=>{
-    console.log(req.body)
-    res.send(`O que você me enviou foi ${req.body.nome}`);
-})
-
+app.use(routes);
 
 app.listen(3000, ()=>{
     console.log("Acessando httpp://localhost:3000");
