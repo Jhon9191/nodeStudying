1adddf8ebfe41c6ec426b335401c55396712067a
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-17 14:03:32 -0300

    Creating user

--- projetoAgenda/src/middlewares/middleware.js
+++ projetoAgenda/src/middlewares/middleware.js
@@ -1,11 +1,10 @@
-exports.middlewareSecundary = (req, res, next) => {
-    res.local.umaVariavelGlobal = "valor da variavel";
-    console.log('Passei no middleware secundary');
+exports.middlewareGlobal = (req, res, next) => {
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
     next();
 };
 
-exports.middlewareGlobal = (req, res, next) => {
-    console.log('Passei no middleware global');
+exports.middlewareSecundary = (req, res, next) => {
     next();
 };
 
