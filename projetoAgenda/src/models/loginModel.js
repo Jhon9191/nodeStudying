0f9a2aa998e41f79f9549774faf6c734213593e8
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-17 17:00:13 -0300

    Validating the registration

--- projetoAgenda/src/models/loginModel.js
+++ projetoAgenda/src/models/loginModel.js
@@ -1,43 +1,52 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const loginSchema = new mongoose.Schema({
-    email: {type: String, required: true},
-    password: {type: String, required: true}
+    email: { type: String, required: true },
+    password: { type: String, required: true }
 });
 
 const loginModel = mongoose.model('Login', loginSchema);
 
 class Login {
-    constructor(body){
+    constructor(body) {
         this.body = body;
         this.errors = [];
         this.user = null;
     }
 
-    async register(){
+    async register() {
         this.validation();
-        if(this.errors.length > 0) return;
-        try{
-            this.user = await loginModel.create(this.body);
-        }catch(err){
-            console.log(err)
-        }
+        if (this.errors.length > 0) return;
+
+        await this.userExists();
+        if (this.errors.length > 0) return;
+
+        const salt = bcrypt.genSaltSync();
+        this.body.password = bcrypt.hashSync(this.body.password, salt);
+
+        this.user = await loginModel.create(this.body);
     }
 
-    validation(){
+    async userExists() {
+        this.user = await loginModel.findOne({ email: this.body.email });
+        if (this.user) this.errors.push('Usuário já existe.');
+    }
+
+    validation() {
         this.cleanUp;
-        if(!validator.isEmail(this.body.email)) {
+        if (!validator.isEmail(this.body.email)) {
             this.errors.push("E-mail inválido");
         }
-        if(this.body.password.length < 3 || this.body.password.length > 50) {
+        if (this.body.password.length < 3 || this.body.password.length > 50) {
             this.errors.push("A senha precisa ter entre 3 e 50 caracteres");
         }
     }
-    
-    cleanUp(){ 
-        for(const key in this.body){
-            if(typeof this.body[key] !== 'string'){
+
+    cleanUp() {
+        for (const key in this.body) {
+            if (typeof this.body[key] !== 'string') {
                 this.body[key] = '';
             }
         }
@@ -46,7 +55,7 @@ class Login {
             email: this.body.email,
             password: this.body.password
         }
-    } 
+    }
 
 }
 
