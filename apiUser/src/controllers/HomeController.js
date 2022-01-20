860166d763cbcd21d1b0656923b3f784ca277a3a
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-20 13:44:56 -0300

    Student routes

--- apiUser/src/controllers/HomeController.js
+++ apiUser/src/controllers/HomeController.js
@@ -2,15 +2,7 @@ import Aluno from '../models/aluno';
 
 class HomeController {
   async index(req, res) {
-    const aluno = await Aluno.create({
-      nome: 'joao',
-      sobrenome: 'antonio',
-      email: 'joao@gmail.com',
-      idade: 22,
-      peso: 85.6,
-      altura: 1.76,
-    });
-    res.json(aluno);
+    res.json('oks');
   }
 }
 
