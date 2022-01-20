3bc6660ddd93cf963d79c75753bdc16b71377adc
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-19 22:14:47 -0300

    Configuring and testing Sequelize

--- apiUser/src/controllers/HomeController.js
+++ apiUser/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const aluno = await Aluno.create({
+      nome: 'joao',
+      sobrenome: 'antonio',
+      email: 'joao@gmail.com',
+      idade: 22,
+      peso: 85.6,
+      altura: 1.76,
     });
+    res.json(aluno);
   }
 }
 
