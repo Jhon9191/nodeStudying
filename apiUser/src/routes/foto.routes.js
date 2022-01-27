579ee21dc677e36fc0360a402307bf9b93e310ba
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-27 09:44:15 -0300

    Handling uploaded files

--- apiUser/src/routes/foto.routes.js
+++ apiUser/src/routes/foto.routes.js
@@ -1,13 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
-import loginRequired from '../middlewares/loginRequired';
 import fotoController from '../controllers/FotoController';
-import multerConfig from '../config/multerConfig';
 
 const router = new Router();
 
-const upload = multer(multerConfig);
-
-router.post('/', upload.single('arquivo'), fotoController.store);
+router.post('/', fotoController.store);
 
 export default router;
