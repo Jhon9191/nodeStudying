37f3f76b77ffcba63754a593b351728c5983c056
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-27 12:43:10 -0300

    Configuring project to deploy

---
+++ apiUser/dist/middlewares/loginRequired.js
@@ -0,0 +1,44 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _jsonwebtoken = require('jsonwebtoken'); var _jsonwebtoken2 = _interopRequireDefault(_jsonwebtoken);
+var _user = require('../models/user'); var _user2 = _interopRequireDefault(_user);
+
+exports. default = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      errors: ['Login required'],
+    });
+  }
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const dados = _jsonwebtoken2.default.verify(token, process.env.TOKEN_SECRET);
+    const { id, email } = dados;
+
+    // CASO MUDE INFORMACOES ELE VERIFICA SE OS EMAILS E ID DO USUARIOS
+    // CORRESPONDEM AO USUARIO LOGADO
+    // CASO USUARIO TROQUE SENHA ELE É DESLOGADO DO SITEMA
+
+    const user = await _user2.default.findOne({
+      where: {
+        id,
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        errors: ['Usuário inválido'],
+      });
+    }
+
+    req.userId = id;
+    req.userEmail = email;
+    return next();
+  } catch (e) {
+    return res.status(401).json({
+      errors: ['Token expirado ou inválido.'],
+    });
+  }
+};
--- apiUser/src/app.js
+++ apiUser/src/app.js
@@ -2,14 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import './src/database';
+import './database';
 import express from 'express';
 import { resolve } from 'path';
-import homeRoutes from './src/routes/home.routes';
-import userRoutes from './src/routes/user.routes';
-import tokenRoutes from './src/routes/token.routes';
-import alunoRoutes from './src/routes/aluno.routes';
-import fotoRoutes from './src/routes/foto.routes';
+import homeRoutes from './routes/home.routes';
+import userRoutes from './routes/user.routes';
+import tokenRoutes from './routes/token.routes';
+import alunoRoutes from './routes/aluno.routes';
+import fotoRoutes from './routes/foto.routes';
 
 class App {
   constructor() {
