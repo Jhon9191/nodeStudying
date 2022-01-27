e041873da36c8f17f435f88814f819b9de85003a
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-27 09:45:18 -0300

    Handling uploaded files

--- apiUser/src/controllers/FotoController.js
+++ apiUser/src/controllers/FotoController.js
@@ -4,10 +4,10 @@ import multerConfig from '../config/multerConfig';
 const upload = multer(multerConfig).single('foto');
 class FotoController {
   async store(req, res) {
-    return upload(req, res, (error) => {
-      if (error) {
+    return upload(req, res, (err) => {
+      if (err) {
         return res.status(400).json({
-          errors: [error.code],
+          errors: [err.code],
         });
       }
 
