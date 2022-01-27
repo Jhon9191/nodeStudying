69f39fff18fa85a236df5de5ef638872c5996d26
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-27 19:47:13 -0300

    Components

--- reactbasics/src/App.js
+++ reactbasics/src/App.js
@@ -1,9 +1,11 @@
-import './App.css'
+import React from 'react';
+import './App.css';
+import Main from './components/Main';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Main />
     </div>
   );
 }
