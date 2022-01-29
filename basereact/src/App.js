52c807d6b3598348ad5ea1fe3b91831a2f2ed166
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-29 08:34:12 -0300

    Application menu

--- basereact/src/App.js
+++ basereact/src/App.js
@@ -1,12 +1,15 @@
 import Login from './pages/Login';
 import GlobalStyle from './styles/GlobalStyles';
+import Header from './components/Header';
 /* eslint-disable react/react-in-jsx-scope */
+
 function App() {
   return (
-    <div>
+    <>
+      <Header />
       <GlobalStyle />
       <Login />
-    </div>
+    </>
   );
 }
 
