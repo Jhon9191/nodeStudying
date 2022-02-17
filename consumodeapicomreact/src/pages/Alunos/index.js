02cda85bd86deff2545e696e140cf6660bad1bdf
Jhon9191 <joaoantonio15161516@gmail.com>
2022-02-16 21:34:16 -0300

    Fetching API Students

--- consumodeapicomreact/src/pages/Alunos/index.js
+++ consumodeapicomreact/src/pages/Alunos/index.js
@@ -1,19 +1,42 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useState } from 'react';
+import { AlunoContainer, ProfilePicture } from './styled';
 import { Container } from '../../styles/GlobalStyles';
-import {} from './styled';
-// import { useDispatch } from 'react-redux';
-// import * as exampleAction from '../../store/modules/example/action';
+import { get } from 'lodash'
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
+import axios from '../../services/axios';
 export default function Alunos() {
-  // const dispatch = useDispatch();
-  // const handleLogin = (e) => {
-  //   e.preventDefault();
 
-  //   dispatch(exampleAction.clicaBotaoRequest());
-  // };
+  const [alunos, setAlunos] = useState([]);
+  React.useEffect(() => {
+    async function loadAlunos() {
+      const res = await axios.get('/alunos');
+      setAlunos(res.data);
+    }
+    loadAlunos();
+  }, []);
 
   return (
     <Container>
       <h1>Alunos</h1>
+      <AlunoContainer>
+        {alunos.map((aluno) => (
+          <div key={String(aluno.id)}>
+            {get(aluno, 'Fotos[0].url', false) ? (
+              <ProfilePicture>
+                <img src={"http://" + aluno.Fotos[0].url} alt="teste" />
+              </ProfilePicture>
+            ) : (
+              <FaUserCircle size={36}/>
+            )}
+            <span>{aluno.nome}</span>
+            <span>{aluno.email}</span>
+            <Link to={`/aluno/${aluno.id}/edit`}><FaEdit size={16}/></Link>
+            <Link to={`/aluno/${aluno.id}/delet`}><FaWindowClose size={16}/></Link>
+          </div>
+        ))}
+      </AlunoContainer>
     </Container>
   );
 }
